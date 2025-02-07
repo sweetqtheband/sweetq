3e880e6660a3130ca17f3b9ee273d5b93bc8c9bb
Jesús García González <jesus.garcia9.contractor@bbva.com>
2025-02-07 20:20:09 +0100

    Updated proxy

--- dashboard/app/(pages)/api/proxy/route.ts
+++ dashboard/app/(pages)/api/proxy/route.ts
@@ -25,7 +25,13 @@ export async function GET(req: NextRequest) {
 
   try {
     const response = await fetch(imageUrl, {
-      headers: { 'User-Agent': 'Mozilla/5.0' }, // Evita bloqueos de Instagram
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        Accept:
+          'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        Referer: 'https://www.instagram.com/',
+      }, // Evita bloqueos de Instagram
     });
 
     if (!response.ok) throw new Error('Failed to fetch image');
