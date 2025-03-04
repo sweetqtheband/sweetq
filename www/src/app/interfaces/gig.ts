6b2a0ac399892c57acb3bd5dd3738941167b623f
Jesús García González <jesus.garcia9.contractor@bbva.com>
2025-03-04 11:07:58 +0100

    Updated current web

--- www/src/app/interfaces/gig.ts
+++ www/src/app/interfaces/gig.ts
@@ -1,16 +1,17 @@
-import {Band} from "./band";
+import { Band } from './band';
 export interface Gig {
-    id: number,
-    date: string,
-    day: string,
-    month: string,
-    city: string,
-    hour: string,
-    venue: string,
-    address?: string,
-    map?: string,
-    bands: Band[],
-    event: string,
-    tickets: string;
-    expired?: boolean
+  id: number;
+  date: string;
+  day: string;
+  month: string;
+  year: string;
+  city: string;
+  hour: string;
+  venue: string;
+  address?: string;
+  map?: string;
+  bands: Band[];
+  event: string;
+  tickets: string;
+  expired?: boolean;
 }
