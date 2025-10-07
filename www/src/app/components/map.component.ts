8e9c4ec12db44f86077f80102661b150f3d63164
Jesús García González <jesus.garcia9.contractor@bbva.com>
2025-10-07 12:54:10 +0200

    Added new gig

--- www/src/app/components/map.component.ts
+++ www/src/app/components/map.component.ts
@@ -1,35 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+@Component({
+  selector: 'map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent {
+  // Show or hide modal map
+  @Input() show: boolean = false;
+  // Map url
+  @Input() url: string = '';
+  @Output() showChange = new EventEmitter<boolean>();
 
-import {    
-    Component,
-    Input,
-    Output,
-    EventEmitter    
-  } from '@angular/core';
-
-
-  @Component({
-    selector: 'map',
-    templateUrl: './map.component.html',  
-  })
-  
-  
-  export class MapComponent {
-
-  
-    // Show or hide modal map
-    @Input() show: boolean = false;
-    // Map url
-    @Input() url: string = '';
-    @Output() showChange = new EventEmitter<boolean>();      
-
-    public onShowChanged() {    
-      this.showChange.emit(this.show);
-    }
-
-    
-    hideMap()
-    {      
-      this.showChange.emit(false);
-    }
+  public onShowChanged() {
+    this.showChange.emit(this.show);
   }
+
+  hideMap() {
+    this.showChange.emit(false);
+  }
+}
